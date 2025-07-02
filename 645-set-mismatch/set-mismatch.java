21b3f7b29882b3859fb47b31b37b2eaa8d6a8ce0
Kunal Raj <kunalraj.kr24@gmail.com>
2025-07-02 17:53:48 +0530

    Time: 2 ms (95.21%) | Memory: 45.4 MB (91.38%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,26 +1,43 @@
+// class Solution {
+//     public int[] findErrorNums(int[] nums) {
+//         Arrays.sort(nums);
+//         int[] arr = new int[2];
+//         int total=0;
+//         int sum=0;
+//         for(int i=0; i<nums.length; i++)
+//         {
+//             total +=i+1;
+//             sum +=nums[i];
+//         }
+
+//         int sub = total-sum;
+
+//         for(int i=1; i<nums.length; i++){
+//             if(nums[i-1]==nums[i])
+//             {
+//                 arr[0]=nums[i];
+//                 arr[1]=nums[i]+sub;
+//             }
+//         }
+
+//         return arr;
+
+//     }
+// }
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
-        int[] arr = new int[2];
-        int total=0;
-        int sum=0;
-        for(int i=0; i<nums.length; i++)
-        {
-            total +=i+1;
-            sum +=nums[i];
+        int[] freq = new int[nums.length + 1]; // index 1 to n
+        for (int num : nums) {
+            freq[num]++;
         }
 
-        int sub = total-sum;
-
-        for(int i=1; i<nums.length; i++){
-            if(nums[i-1]==nums[i])
-            {
-                arr[0]=nums[i];
-                arr[1]=nums[i]+sub;
-            }
+        int[] result = new int[2]; // [duplicate, missing]
+        for (int i = 1; i <= nums.length; i++) {
+            if (freq[i] == 0) result[1] = i;     // missing
+            else if (freq[i] == 2) result[0] = i; // duplicate
         }
 
-        return arr;
-
+        return result;
     }
 }
