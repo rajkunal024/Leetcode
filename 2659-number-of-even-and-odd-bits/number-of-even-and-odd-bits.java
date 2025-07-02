5df556b0ce4849a28697484f113f99a25d22cb92
Kunal Raj <kunalraj.kr24@gmail.com>
2025-07-02 15:58:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (97.75%) - LeetSync

--- 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.java
+++ 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.java
@@ -10,3 +10,23 @@ class Solution {
         return ans;
     }
 }
+
+//Brute Force
+
+// class Solution {
+//     public int[] evenOddBit(int n) {
+//         int odd=0;
+//         int even=0;
+//         String binary = Integer.toBinaryString(n);
+//         for(int i=0; i<binary.length(); i++)
+//         {
+//             if (binary.charAt(binary.length() - 1 - i) == '1')
+//             {
+//                 if(i%2==0) even++;
+//                 else odd++;
+//             }
+//         }
+//         int[] ans = {even, odd};
+//         return ans;
+//     }
+// }
