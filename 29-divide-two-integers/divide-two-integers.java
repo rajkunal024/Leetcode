cfe1377240cfebf0208e686a83d549217dc38abb
Kunal Raj <kunalraj.kr24@gmail.com>
2025-07-08 10:27:20 +0530

    Time: 1 ms (74.18%) | Memory: 41.2 MB (35.42%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,27 +1,26 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
-        boolean sign = true;
-        if(dividend < 0 && divisor > 0) sign = false;
-        if(dividend >= 0 && divisor < 0) sign = false;
-        if(dividend < 0 && divisor < 0) sign = true;
-        long n = Math.abs((long)dividend);
-        long d = Math.abs((long)divisor);
-        int ans=0;
-        // if(dividend >= Math.pow(2, 31) && sign == true && d == 1) return Integer.MAX_VALUE;
-        // if(dividend <= Math.pow(-2, 31) && sign == false && d == 1) return Integer.MIN_VALUE;
-        // if(dividend <= Math.pow(-2, 31) && sign == true && d == 1) return Integer.MAX_VALUE;
-        // if(dividend >= Math.pow(2, 31) && sign == false && d == 1) return Integer.MIN_VALUE;
-        while(n>=d)
-        {
-            int count =0;
-            while(n >= (d << ( count + 1)))
-            {
+        if (dividend == divisor) return 1;
+
+        boolean sign = (dividend < 0) == (divisor < 0);
+
+        long n = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
+        long ans = 0;
+
+        while (n >= d) {
+            int count = 0;
+            while (n >= (d << (count + 1))) {
                 count++;
             }
-            ans += (1 << count);
-            n =n -(d<<count);
+            ans += (1L << count);
+            n -= (d << count);
         }
-        return sign ? ans : (-1*ans);
+
+        if (ans >= (1L << 31)) {
+            return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+
+        return (int)(sign ? ans : -ans);
     }
 }
